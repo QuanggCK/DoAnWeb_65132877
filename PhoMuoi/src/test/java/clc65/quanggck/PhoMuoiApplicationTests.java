b15d6857f80cd019c4e39097d631af435a467ad7
QuanggCK <zaphkiel1610@gmail.com>
2026-04-30 17:23:29 +0700

    first commit

---
+++ PhoMuoi/src/test/java/clc65/quanggck/PhoMuoiApplicationTests.java
@@ -0,0 +1,13 @@
+package clc65.quanggck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoMuoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
