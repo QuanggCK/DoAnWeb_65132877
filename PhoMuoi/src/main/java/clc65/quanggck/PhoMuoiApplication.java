5e5dc7198af4a04d2a3e24e30132965b8cdb20c1
QuanggCK <zaphkiel1610@gmail.com>
2026-05-20 19:38:21 +0700

    revamped 1.0

---
+++ PhoMuoi/src/main/java/clc65/quanggck/PhoMuoiApplication.java
@@ -0,0 +1,13 @@
+package clc65.quanggck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoMuoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoMuoiApplication.class, args);
+	}
+
+}
