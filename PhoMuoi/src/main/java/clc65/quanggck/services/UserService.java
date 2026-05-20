f8cdd28ae778cced520cd4e4b1187b4a2aab54ce
QuanggCK <zaphkiel1610@gmail.com>
2026-05-20 13:05:56 +0700

    update

--- PhoMuoi/src/main/java/clc65/quanggck/services/UserService.java
+++ PhoMuoi/src/main/java/clc65/quanggck/services/UserService.java
@@ -1,38 +1,38 @@
 package clc65.quanggck.services;
 
 import clc65.quanggck.models.User;
-import clc65.quanggck.repos.UserRepository;
-import lombok.RequiredArgsConstructor;
+import clc65.quanggck.repos.UserRepository; 
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
 
-    // Lấy tất cả người dùng (Dành cho Admin)
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     // Đăng ký tài khoản mới
     public User register(User user) {
-        // Kiểm tra xem số điện thoại đã tồn tại chưa
         if (userRepository.findBySdt(user.getSdt()) != null) {
             throw new RuntimeException("Số điện thoại này đã được đăng ký!");
         }
-        // Mặc định tài khoản mới tạo không phải là admin
         user.setRoleAdmin(false); 
         return userRepository.save(user);
     }
 
-    // Đăng nhập đơn giản
+
     public User login(String sdt, String matKhau) {
         User user = userRepository.findBySdt(sdt);
         if (user != null && user.getMatKhau().equals(matKhau)) {
-            return user; // Đăng nhập thành công
+            return user; 
         }
         throw new RuntimeException("Số điện thoại hoặc mật khẩu không chính xác!");
     }
