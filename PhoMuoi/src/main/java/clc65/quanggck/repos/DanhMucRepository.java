d9778129dff3bd16ad4288f1a1577dbf82c89821
QuanggCK <zaphkiel1610@gmail.com>
2026-05-20 19:37:08 +0700

    delete all

--- PhoMuoi/src/main/java/clc65/quanggck/repos/DanhMucRepository.java
+++
@@ -1,9 +0,0 @@
-package clc65.quanggck.repos;
-
-import clc65.quanggck.models.DanhMuc;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DanhMucRepository extends JpaRepository<DanhMuc, Integer> {
-}
