5b0c0a09ddf595d8ca88dd123fe0aeec7aa6547d
QuanggCK <zaphkiel1610@gmail.com>
2026-05-20 13:05:17 +0700

    update

--- PhoMuoi/src/main/java/clc65/quanggck/models/User.java
+++ PhoMuoi/src/main/java/clc65/quanggck/models/User.java
@@ -1,15 +1,11 @@
 package clc65.quanggck.models;
 
 import jakarta.persistence.*;
-import lombok.*;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
 
     @Id
@@ -36,4 +32,73 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonIgnore
     private List<DonHang> dsDonHang;
+
+    public User() {
+    }
+
+    public User(Integer userId, String tenKhach, String sdt, String diaChi, Boolean roleAdmin, String matKhau, List<DonHang> dsDonHang) {
+        this.userId = userId;
+        this.tenKhach = tenKhach;
+        this.sdt = sdt;
+        this.diaChi = diaChi;
+        this.roleAdmin = roleAdmin;
+        this.matKhau = matKhau;
+        this.dsDonHang = dsDonHang;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getTenKhach() {
+        return tenKhach;
+    }
+
+    public void setTenKhach(String tenKhach) {
+        this.tenKhach = tenKhach;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public Boolean getRoleAdmin() {
+        return roleAdmin;
+    }
+
+    public void setRoleAdmin(Boolean roleAdmin) {
+        this.roleAdmin = roleAdmin;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public List<DonHang> getDsDonHang() {
+        return dsDonHang;
+    }
+
+    public void setDsDonHang(List<DonHang> dsDonHang) {
+        this.dsDonHang = dsDonHang;
+    }
 }
